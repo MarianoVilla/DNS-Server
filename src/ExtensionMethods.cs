d93aed92a3cfa735e9e377639108018a812e227d
MarianoVilla <dager_a7x@hotmail.com>
2024-05-11 21:18:33 -0300

    Minor logging tweak

--- src/ExtensionMethods.cs
+++ src/ExtensionMethods.cs
@@ -16,12 +16,8 @@ namespace codecrafters_dns_server.src
         public static void LogBytes(this ILogger Logger, byte[] Bytes, 
             string? Title = null, LogLevel Level = LogLevel.Information) 
         {
-            Logger.Log(Level, $"************{Title}************");
-            for (int i = 0; i < Bytes.Count(); i++)
-            {
-                Logger.Log(Level, $"[{i}]: 0x{Bytes.ElementAt(i):X2}, ");
-            }
-            Logger.Log(Level, "\n");
+            Logger.Log(Level, $"************{Title}************\n" +
+                string.Join(", ", Bytes.Select((b, i) => $"[{i}]: 0x{b:X2}, ")));
         }
 
     }
